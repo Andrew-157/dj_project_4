7c263a0b2a7e2b4184abb4c9c0719f9c63bdc682
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-25 13:03:45 +0200

    Category model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4,
+        editable=False)
+    title = models.CharField(max_length=255,
+                             validators=[MinLengthValidator(3)],
+                             null=False, unique=True)
