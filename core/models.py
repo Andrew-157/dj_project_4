faebb58144edb907ac8b0e75a7c338cc248f149f
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-27 14:45:56 +0200

    is_ready boolean field on Article

--- core/models.py
+++ core/models.py
@@ -32,6 +32,7 @@ class Article(models.Model):
         'core.Category', on_delete=models.PROTECT, related_name='articles')
     published = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True)
+    is_ready = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.id) + ' ' + self.title
