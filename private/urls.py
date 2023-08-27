3097437042eb186a8a67b1ae125b18a115e598d5
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-27 10:54:33 +0200

    Update article View

--- private/urls.py
+++ private/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('you/', views.PrivatePageView.as_view(), name='private-page'),
     path('you/publish/', views.PostArticleView.as_view(), name='post-article'),
     path('you/articles/<uuid:id>/',
-         views.ArticleDetailView.as_view(), name='article-detail')
+         views.ArticleDetailView.as_view(), name='article-detail'),
+    path('you/articles/<uuid:id>/update/',
+         views.UpdateArticleView.as_view(), name='update-article')
 ]
--- private/views.py
+++ private/views.py
@@ -1,8 +1,9 @@
-from typing import Any, Optional
+from typing import Any, Dict, Optional
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import models
+from django.forms.models import BaseModelForm
 from django.urls import converters
 from django.http import HttpResponseForbidden
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
@@ -11,7 +12,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.generic import FormView, DetailView
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 
@@ -37,8 +38,6 @@ class PrivatePageView(View):
 class PostArticleView(LoginRequiredMixin, CreateView):
     template_name = 'private/publish_article.html'
     form_class = CreateArticleForm
-    success_url = reverse_lazy('private:article-detail',
-                               )
     model = Article
 
     def form_valid(self, form) -> HttpResponse:
@@ -56,6 +55,8 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'article'
 
     def get_object(self):
+        # Getting object like this is necessary because default implementation
+        # does not understand uuid as id
         return get_object_or_404(Article, id=self.kwargs['id'])
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
@@ -63,3 +64,27 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
         if object.author != request.user:
             raise PermissionDenied
         return super().get(request, *args, **kwargs)
+
+
+class UpdateArticleView(LoginRequiredMixin, UpdateView):
+    template_name = 'private/update_article.html'
+    form_class = CreateArticleForm
+    model = Article
+
+    def get_object(self):
+        article = get_object_or_404(Article, id=self.kwargs['id'])
+        if article.author != self.request.user:
+            raise PermissionDenied
+        return article
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        self.object: Article = form.save()
+        messages.success(
+            self.request, 'You successfully updated your article!')
+        return HttpResponseRedirect(reverse('private:article-detail',
+                                            kwargs={'id': self.object.id}))
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['article'] = self.object
+        return context
