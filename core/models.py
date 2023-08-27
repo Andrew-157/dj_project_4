817ebc3c3df63c00e1c4161aa139522574fd6f0c
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-27 11:04:35 +0200

    Section Model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
+from typing import Iterable, Optional
 import uuid
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.template.defaultfilters import slugify
 
 from users.models import CustomUser
 
@@ -32,3 +34,21 @@ class Article(models.Model):
 
     def __str__(self):
         return self.id + ' ' + self.title
+
+
+class Section(models.Model):
+    title = models.CharField(max_length=255,
+                             validators=[MinLengthValidator(5)],
+                             null=False)
+    slug = models.CharField(max_length=300,
+                            null=False)
+    article = models.ForeignKey('core.Article', on_delete=models.CASCADE)
+    published = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Section, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
