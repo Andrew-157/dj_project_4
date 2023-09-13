22e1c4bb99cffe00a29d2aaf9a3ef35c6e433188
Andrew Subotin <subotinandrey5@gmail.com>
2023-09-13 11:25:01 +0200

    Placeholders in inputs of forms for publishing articles and sections

--- private/forms.py
+++ private/forms.py
@@ -10,11 +10,13 @@ from core.models import Article, Section
 class CreateUpdateArticleForm(forms.ModelForm):
     title = forms.CharField(help_text="Title may be something like this: Ionic compounds' role in nutrition.",
                             max_length=255,
-                            min_length=5)
+                            min_length=5,
+                            widget=forms.TextInput(attrs={'placeholder': 'Enter title for your article'}))
     tags_string = forms.CharField(help_text="Enter tags, separating them with comma.#-sign is not necessary.",
                                   max_length=500,
                                   min_length=2,
-                                  label='Tags', required=False)
+                                  label='Tags', required=False,
+                                  widget=forms.TextInput(attrs={'placeholder': 'Enter some tags'}))
 
     class Meta:
         model = Article
@@ -30,3 +32,7 @@ class CreateUpdateSectionForm(forms.ModelForm):
     class Meta:
         model = Section
         fields = ['title', 'number', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter title of the section'}),
+            'content': forms.Textarea(attrs={'placeholder': 'Enter content of the section'})
+        }
